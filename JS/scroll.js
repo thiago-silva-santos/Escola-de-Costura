539e18ba4e3120d3ae6cfb6c2cc450499b994ca9
thiago-silva-santos <thiagomj98@gmail.com>
2021-02-20 18:40:19 -0300

    Consertando scroll to top

--- JS/scroll.js
+++ JS/scroll.js
@@ -4,7 +4,12 @@ const toTop = document.querySelector(".to-top");
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 2000) {
     toTop.classList.add("active");
+    toTop.addEventListener('click', () => window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      }))
   } else {
     toTop.classList.remove("active");
   }
 })
+
